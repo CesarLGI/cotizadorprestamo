1fb6a65667d556e5ea8d7abcba4fbfc2f811d4e6
Cesar Cisnero <cesarlgi01@gmail.com>
2024-02-01 15:53:00 -0300

    Test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,42 @@
 import React from 'react';
-import {View,Text,StyleSheet,TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import colors from '../utils/colors';
 
-const Footer=(props)=>{
-    const {calcular}=props;
-    return(
-        <View style={styles.viewForm}>
-            <TouchableOpacity style={styles.boton} onPress={()=>calcular()}>
-            <   Text style={styles.text}>Calcular</Text>
-            </TouchableOpacity>
-            
-        </View>
-    );
-}
-const styles=StyleSheet.create({
-    viewForm:{
-        position:'absolute',
-        bottom:0,
-        width:'100%',
-        backgroundColor:colors.PRIMARY_COLOR,
-        height:100,
-        borderTopLeftRadius:30,
-        borderTopRightRadius:30,
-        alignItems:'center',
-        justifyContent:'center'
-        
-    },
-    boton:{
-        borderRadius:20,
-        backgroundColor:colors.PRIMARY_COLOR_DARK,
-        padding:16,
-        width:'75%'
-    },
-    text:{
-        fontWeight:'bold',
-        fontSize:18,
-        color:'#fff',
-        textTransform:'uppercase',
-        textAlign:'center'
-
-    }
-})
+const Footer = (props) => {
+  console.log('hola q tal');
+  const {calcular} = props;
+  return (
+    <View style={styles.viewForm}>
+      <TouchableOpacity style={styles.boton} onPress={() => calcular()}>
+        <Text style={styles.text}>Calcular</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  viewForm: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    backgroundColor: colors.PRIMARY_COLOR,
+    height: 100,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  boton: {
+    borderRadius: 20,
+    backgroundColor: colors.PRIMARY_COLOR_DARK,
+    padding: 16,
+    width: '75%',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#fff',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+  },
+});
 export default Footer;
